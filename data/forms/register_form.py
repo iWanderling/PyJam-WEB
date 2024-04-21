c640afd89f4ea0136de5815503cdb62b67f21076
iWanderling <slyvkanikitao@gmail.com>
2024-04-21 13:05:57 +0500

    Добавление новых функций в ЛК: Изменение данных, Изменение пароля, Выход из аккаунта; Заготовка для топа пользователей

--- data/forms/register_form.py
+++ data/forms/register_form.py
@@ -1,13 +1,14 @@
-from wtforms import PasswordField, StringField, IntegerField, SubmitField, EmailField
+from wtforms import PasswordField, StringField, IntegerField, SubmitField, EmailField, SelectField
+from data.forms.gender_dropdown_field_form import Gender
 from wtforms.validators import DataRequired
 from flask_wtf import FlaskForm
 
 
-# Класс для регистрации пользователя
 class RegisterForm(FlaskForm):
     email = EmailField('Электронная почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
     surname = StringField('Фамилия', validators=[DataRequired()])
     name = StringField('Имя', validators=[DataRequired()])
+    gender = SelectField('Пол', choices=['Мужской', 'Женский'])
     submit = SubmitField('Регистрация')
